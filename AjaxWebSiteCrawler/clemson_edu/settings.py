ecce01377c494e27014fd9e42bb723b14943df18
srinathreddy-1206 <srinathreddy.1206@gmail.com>
2014-10-02 21:52:45 +0530

    I Decoded the ajax calls and used that api directly while crawling

---
+++ AjaxWebSiteCrawler/clemson_edu/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for clemson_edu project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'clemson_edu'
+
+SPIDER_MODULES = ['clemson_edu.spiders']
+NEWSPIDER_MODULE = 'clemson_edu.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'clemson_edu (+http://www.yourdomain.com)'
